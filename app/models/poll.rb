eb60b927b3f470af56e265da089aa39fd0fa6262
B Kleinen <kleinen@infrastructure.de>
2016-10-28 18:39:48 +0200

    poll creation done

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -1,4 +1,15 @@
 class Poll < ApplicationRecord
-  has_many :answers
-  has_many :participation_tokens
+  has_many :answers #, dependent: :destroy
+  has_many :participation_tokens, dependent: :destroy
+  after_create :generate_participation_tokens
+
+  validates :name, :participants, presence: true
+  validates :participants, numericality: true
+
+
+  def generate_participation_tokens
+    participants.times do
+      participation_tokens.create(participation_key: SecureRandom.urlsafe_base64)
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   resources :numbers
   resources :participation_tokens
   resources :polls
+
+  root to: 'polls#index'
+
+  get 'polls/:id/participate/:participation_token', to: 'polls#participate', as: 'participitation'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
