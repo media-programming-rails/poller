552d778d49839599ebc69bd609b544cf741c987c
B Kleinen <kleinen@infrastructure.de>
2016-10-30 09:49:18 +0100

    moved PollValidator to own file in app/models

--- app/models/number.rb
+++ app/models/number.rb
@@ -1,5 +1,3 @@
-require 'poll'
-
 class Number < ApplicationRecord
   belongs_to :poll
   validates_with PollValidator
--- app/models/poll.rb
+++ app/models/poll.rb
@@ -35,16 +35,3 @@ class Poll < ApplicationRecord
     end
   end
 end
-
-class PollValidator < ActiveModel::Validator
-  def validate(record)
-    poll = record.poll
-    unless record.poll.valid_participation_token?(record.participation_key)
-      record.errors[:poll] << 'Invalid participation token for this poll'
-    else
-      unless poll.register_answer(record)
-        record.errors[:poll] << 'Answer could not be registered with this poll'
-      end
-    end
-  end
-end
