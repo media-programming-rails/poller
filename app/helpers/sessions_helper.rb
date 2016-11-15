41b22db2c9a3c04aef0e7cccd8120e2177e41141
B Kleinen <kleinen@infrastructure.de>
2016-11-15 09:28:11 +0100

    locate bug that current_user method toggled @current_user

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -7,10 +7,25 @@ module SessionsHelper
     @current_user = nil
   end
   def current_user
-    @current_user = @current_user || session[:user_id].nil? ? nil : User.find_by(id: session[:user_id])
+    logger.debug "Called current_user - 1#{@current_user} session[:user_id] #{session[:user_id]}"
+    logger.debug "@current_user: #{(@current_user ?  'true' : 'false')}"
+    # that was incorrect because it toggled the current user! - || has precedence over ?
+    # @current_user = @current_user || session[:user_id].nil? ? nil : User.find_by(id: session[:user_id])
+    # thus, either with explicit parantheses:
+    # @current_user = @current_user || (session[:user_id].nil? ? nil : User.find_by(id: session[:user_id]))
+    # or, using the correct ruby idiom:
+    @current_user ||= session[:user_id].nil? ? nil : User.find_by(id: session[:user_id])
+    logger.debug "Called current_user - 2#{@current_user} session[:user_id] #{session[:user_id]}"
+    logger.debug "@current_user: #{(@current_user ?  'true' : 'false')}"
+    return @current_user
   end
   def logged_in?
-    !current_user.nil?
+    logger.debug "Called logged_in -1#{@current_user} session[:user_id] #{session[:user_id]}"
+    logger.debug "@current_user: #{(@current_user ?  'true' : 'false')}"
+    result = !current_user.nil?
+    logger.debug "Called logged_in -2#{@current_user} - result #{result} session[:user_id] #{session[:user_id]}"
+    logger.debug "@current_user: #{(@current_user ?  'true' : 'false')}"
+    return result
   end
 protected
   def authorize
